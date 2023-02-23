2078f0fe5758e1524744b1247308f25dc9dabbd3
David <david.kesselmann@gmail.com>
2023-02-23 21:02:41 +0100

    Started working on Playlist Support

--- src/dev/redcodes/euklid/Euklid.java
+++ src/dev/redcodes/euklid/Euklid.java
@@ -35,196 +35,197 @@ import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class Euklid {
 
-	private static JDA jda;
+    private static JDA jda;
 
-	private static Logger logger = LoggerFactory.getLogger(Euklid.class);
+    private static Logger logger = LoggerFactory.getLogger(Euklid.class);
 
-	private static String version = "Preview 1.2";
+    private static String version = "Preview 1.2";
 
-	private static boolean dev = false;
+    private static boolean dev = false;
 
-	private static String year;
+    private static String year;
 
-	private static String icon = "https://i.imgur.com/xARAVsM.png";
+    private static String icon = "https://i.imgur.com/xARAVsM.png";
 
-	private static Instant online = Instant.now();
+    private static Instant online = Instant.now();
 
-	private static String episodeData = "https://raw.githubusercontent.com/Lobby-Hoes/hobbylos-data/main/folgen.json";
-	private static String mathefactData = "https://raw.githubusercontent.com/Lobby-Hoes/hobbylos-data/main/mathefacts.json";
-	private static String storyData = "https://raw.githubusercontent.com/Lobby-Hoes/hobbylos-data/main/staedtegeschichten.json";
+    private static String episodeData = "https://raw.githubusercontent.com/Lobby-Hoes/hobbylos-data/main/folgen.json";
+    private static String mathefactData = "https://raw.githubusercontent.com/Lobby-Hoes/hobbylos-data/main/mathefacts.json";
+    private static String storyData = "https://raw.githubusercontent.com/Lobby-Hoes/hobbylos-data/main/staedtegeschichten.json";
+    private static String playlistData = "https://raw.githubusercontent.com/Lobby-Hoes/hobbylos-data/main/playlists.json";
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		String token = args[0];
+        String token = args[0];
 
-		JDABuilder builder = JDABuilder.createDefault(token);
+        JDABuilder builder = JDABuilder.createDefault(token);
 
-		builder.setActivity(Activity.watching("Bot starting..."));
-		builder.setStatus(OnlineStatus.IDLE);
+        builder.setActivity(Activity.watching("Bot starting..."));
+        builder.setStatus(OnlineStatus.IDLE);
 
-		List<GatewayIntent> intents = new ArrayList<>();
-		intents.addAll(GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS));
-		intents.remove(GatewayIntent.GUILD_PRESENCES);
+        List<GatewayIntent> intents = new ArrayList<>();
+        intents.addAll(GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS));
+        intents.remove(GatewayIntent.GUILD_PRESENCES);
 
-		builder.setEnabledIntents(intents);
-		builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+        builder.setEnabledIntents(intents);
+        builder.setMemberCachePolicy(MemberCachePolicy.ALL);
 
-		builder.addEventListeners(new MathefactAutoComplete());
-		builder.addEventListeners(new MathefactCommand());
-		builder.addEventListeners(new InfoCommand());
-		builder.addEventListeners(new StadtGeschichteAutoComplete());
-		builder.addEventListeners(new StadtGeschichteCommand());
+        builder.addEventListeners(new MathefactAutoComplete());
+        builder.addEventListeners(new MathefactCommand());
+        builder.addEventListeners(new InfoCommand());
+        builder.addEventListeners(new StadtGeschichteAutoComplete());
+        builder.addEventListeners(new StadtGeschichteCommand());
 
-		try {
-			jda = builder.build();
-		} catch (LoginException e) {
-			e.printStackTrace();
-		}
+        jda = builder.build();
 
-		year = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
+        year = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
 
-		logger.info("The bot is now online!");
+        logger.info("The bot is now online!");
 
-		shutdown();
-		runLoop();
+        shutdown();
+        runLoop();
 
-	}
+    }
 
-	private static boolean shutdown = false;
+    private static boolean shutdown = false;
 
-	private static void shutdown() {
+    private static void shutdown() {
 
-		new Thread(() -> {
+        new Thread(() -> {
 
-			String line = "";
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String line = "";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-			try {
-				while ((line = reader.readLine()) != null) {
-					if (line.equalsIgnoreCase("exit") || line.equalsIgnoreCase("stop")) {
-						shutdown = true;
-						if (jda != null) {
-							jda.getPresence().setStatus(OnlineStatus.OFFLINE);
-							jda.shutdown();
-							logger.info("The bot is now offline!");
-						}
-						reader.close();
-						System.exit(0);
-						break;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    if (line.equalsIgnoreCase("exit") || line.equalsIgnoreCase("stop")) {
+                        shutdown = true;
+                        if (jda != null) {
+                            jda.getPresence().setStatus(OnlineStatus.OFFLINE);
+                            jda.shutdown();
+                            logger.info("The bot is now offline!");
+                        }
+                        reader.close();
+                        System.exit(0);
+                        break;
 
-					} else {
-						logger.warn("Unknown Command \"" + line + "\"");
-					}
-				}
+                    } else {
+                        logger.warn("Unknown Command \"" + line + "\"");
+                    }
+                }
 
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
 
-		}).start();
-	}
+        }).start();
+    }
 
-	private static void runLoop() {
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
+    private static void runLoop() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
 
-			@Override
-			public void run() {
-				if (!shutdown) {
-					onSecond();
-				}
-			}
-		}, 2000, (15 * 1000));
-	}
+            @Override
+            public void run() {
+                if (!shutdown) {
+                    onSecond();
+                }
+            }
+        }, 2000, (15 * 1000));
+    }
 
-	private static boolean commandCheck = true;
-	private static String[] status = new String[] { "mit euklid", "%members% Users", "%version%" };
-	private static Random rand = new Random();
+    private static boolean commandCheck = true;
+    private static String[] status = new String[]{"mit euklid", "%members% Users", "%version%"};
+    private static Random rand = new Random();
 
-	private static void onSecond() {
+    private static void onSecond() {
 
-		if (commandCheck) {
-			commandCheck = false;
+        if (commandCheck) {
+            commandCheck = false;
 
-			List<CommandData> cmds = new ArrayList<>();
+            List<CommandData> cmds = new ArrayList<>();
 
-			cmds.add(Commands.slash("mathefact", "Suche nach einem Mathefact aus dem Hobbylos-Podcast.")
-					.addOptions(new OptionData(OptionType.STRING, "suche",
-							"Suche nach der Folgennummer, Folgenname oder dem Mathefactthema.", true)
-							.setAutoComplete(true)));
-			cmds.add(Commands.slash("stadtgeschichte", "Suche nach einer Stadtgeschichte aus dem Hobbylos-Podcast.")
-					.addOptions(new OptionData(OptionType.STRING, "suche",
-							"Suche nach der Folgennummer, Folgenname oder dem Titel der Stadtgeschichte.", true)
-							.setAutoComplete(true)));
-			cmds.add(Commands.slash("info", "Information über den Bot."));
+            cmds.add(Commands.slash("mathefact", "Suche nach einem Mathefact aus dem Hobbylos-Podcast.")
+                    .addOptions(new OptionData(OptionType.STRING, "suche",
+                            "Suche nach der Folgennummer, Folgenname oder dem Mathefactthema.", true)
+                            .setAutoComplete(true)));
+            cmds.add(Commands.slash("stadtgeschichte", "Suche nach einer Stadtgeschichte aus dem Hobbylos-Podcast.")
+                    .addOptions(new OptionData(OptionType.STRING, "suche",
+                            "Suche nach der Folgennummer, Folgenname oder dem Titel der Stadtgeschichte.", true)
+                            .setAutoComplete(true)));
+            cmds.add(Commands.slash("info", "Information über den Bot."));
 
-			if (dev) {
-				jda.getGuildById(580732235313971211L).updateCommands().addCommands(cmds)
-						.queue(commands -> logger.info("Commands published"));
-				jda.updateCommands().addCommands(new ArrayList<>()).queue();
-			} else {
-				jda.updateCommands().addCommands(cmds).queue(commands -> logger.info("Commands published"));
-				jda.getGuildById(580732235313971211L).updateCommands().addCommands(new ArrayList<>()).queue();
-			}
+            if (dev) {
+                jda.getGuildById(580732235313971211L).updateCommands().addCommands(cmds)
+                        .queue(commands -> logger.info("Commands published"));
+                jda.updateCommands().addCommands(new ArrayList<>()).queue();
+            } else {
+                jda.updateCommands().addCommands(cmds).queue(commands -> logger.info("Commands published"));
+                jda.getGuildById(580732235313971211L).updateCommands().addCommands(new ArrayList<>()).queue();
+            }
 
-		}
+        }
 
-		int i = rand.nextInt(status.length);
+        int i = rand.nextInt(status.length);
 
-		int users = 0;
+        int users = 0;
 
-		for (Guild guild : jda.getGuilds()) {
-			users = users + guild.getMemberCount();
-		}
+        for (Guild guild : jda.getGuilds()) {
+            users = users + guild.getMemberCount();
+        }
 
-		String text = status[i].replace("%members%", String.valueOf(users)).replace("%version%", version)
-				.replace("%guilds%", String.valueOf(jda.getGuilds().size()));
+        String text = status[i].replace("%members%", String.valueOf(users)).replace("%version%", version)
+                .replace("%guilds%", String.valueOf(jda.getGuilds().size()));
 
-		if (!jda.getPresence().getActivity().getName().equals(text)) {
+        if (!jda.getPresence().getActivity().getName().equals(text)) {
 
-			if (!dev) {
-				if (text.contains("axolotl")) {
-					jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.listening(text));
-				} else if (text.contains("User") || text.contains("Guilds") || text.contains("axolotl")) {
-					jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.watching(text));
-				} else {
-					jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.playing(text));
-				}
-			} else {
-				jda.getPresence().setPresence(OnlineStatus.DO_NOT_DISTURB, Activity.watching("Development"));
-			}
-		} else {
-			onSecond();
-		}
+            if (!dev) {
+                if (text.contains("axolotl")) {
+                    jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.listening(text));
+                } else if (text.contains("User") || text.contains("Guilds") || text.contains("axolotl")) {
+                    jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.watching(text));
+                } else {
+                    jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.playing(text));
+                }
+            } else {
+                jda.getPresence().setPresence(OnlineStatus.DO_NOT_DISTURB, Activity.watching("Development"));
+            }
+        } else {
+            onSecond();
+        }
 
-	}
+    }
 
-	public static String getYear() {
-		return year;
-	}
+    public static String getYear() {
+        return year;
+    }
 
-	public static String getIconUrl() {
-		return icon;
-	}
+    public static String getIconUrl() {
+        return icon;
+    }
 
-	public static Instant getOnlineSince() {
-		return online;
-	}
+    public static Instant getOnlineSince() {
+        return online;
+    }
 
-	public static String getVersion() {
-		return version;
-	}
+    public static String getVersion() {
+        return version;
+    }
 
-	public static String getEpisodeDataUrl() {
-		return episodeData;
-	}
+    public static String getEpisodeDataUrl() {
+        return episodeData;
+    }
 
-	public static String getMathefactDataUrl() {
-		return mathefactData;
-	}
+    public static String getMathefactDataUrl() {
+        return mathefactData;
+    }
 
-	public static String getStoryDataUrl() {
-		return storyData;
-	}
+    public static String getStoryDataUrl() {
+        return storyData;
+    }
+
+    public static String getPlaylistDataUrl() {
+        return playlistData;
+    }
 
 }
